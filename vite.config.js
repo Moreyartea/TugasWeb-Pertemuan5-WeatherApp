813fdd0c0673d7bc21d8cb4c91066d26c1cbc436
Moreyartea <fatihtaqiyyuddin113@gmail.com>
2026-09-11 16:20:19 +0700

    -m "Configure Cloudflare Workers"

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
+import { cloudflare } from '@cloudflare/vite-plugin';
 
 export default defineConfig({
+  plugins: [
+    cloudflare(),
+  ],
   base: './',
   server: {
     port: 5173,
